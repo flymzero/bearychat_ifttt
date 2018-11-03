cff842466ca07fff5834302e3ae25a0d32e34c82
mzero <z@z.com>
2018-11-03 12:52:28 +0800

    add imgs

--- rtm/bc/message.go
+++ rtm/bc/message.go
@@ -20,33 +20,15 @@ const (
 	DoType   MessageType = "$"   //触发操作
 )
 
-const helpText = `![tu]()
-[bearychat_ifttt](https://github.com/flymzero/bearychat_ifttt) 使用帮助
+const helpText = `![](https://raw.githubusercontent.com/flymzero/bearychat_ifttt/master/imgs/i2b.png)
+> 具体使用教程 : [链接](https://www.v2ex.com/t/503333#reply1)
+> ifttt相关文章: [链接](https://sspai.com/post/39243?utm_source=weibo&utm_medium=sspai&utm_campaign=weibo&utm_content=ifttt&utm_term=jiaocheng)
+> ifttt获取key: [链接](http://maker.ifttt.com/)
 
 **更新**
-
+> 增加反向webhook,触发ifttt服务>机器人>具体内容到通知倍洽用户
+> 正向webhook,可以引用消息附件
 > 去除email数据绑定，一个对象只绑定**名称**和**key**，名称唯一
-
-**对象数据绑定操作**
-
-把自己或者别人（统一称为对象）先绑定对应的ifttt的key执行操作
-一个对象包含**名称**和**key**两个字段 （名称唯一）
-
-> **-h**    获取使用帮助
-> **-ls**   列出所有自己及自己设置对象的信息(名称&key)
-> **-s [-m]  n:名称  k:ifttt上的key **   设置自己或者对象的信息(n必填唯一，-m则表示设置自己信息)
-- 例1 : -s -m n:我 k:xxxxxx  设置自己的名称称和ifttt的key
-- 例2 : -s n:老婆 k:oooooo 设置一个对象：老婆及触发的key
-> **-d 名称**  删除你对象中对应名称的这个对象
-- 例 : -d 老婆 把老婆这个对象删了
-
-**触发操作 ifttt webhooks 触发词 + 任意服务**
-> **$触发词  [n:名称]  [v1:xx]  [v2:xx]  [v3:xx] ** 
-对这个名称对象(n不填就是自己，v1,v2,v3都是可选的)，进行触发操作,并传输可选的3个参数，~~当存在引用附件时以附件的url优先作为v3的值~~,贝洽的文件在未登录的情况下无法访问链接,所以无法转存!!!
-需在"对象"手机ifttt上创建对应的Applet
-
-**基本上ifttt上能创造的东西(通知,发邮件,文件转存,发微博....),你都可以让这个机器人代劳, 话说ifttt不开放共享applet也真是坑**
-更多使用方法见 https://github.com/flymzero/bearychat_ifttt
 `
 
 const noUserInfo = "没有关于您的用户信息请先设置 命令：**-s**"
